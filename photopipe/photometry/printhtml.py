1ef655effaed906295a7c10661ec576c430f656b
maxperry <maxperryperinato+github@gmail.com>
2018-08-16 13:47:16 +0900

    removed unused functions removed suptitle from SEDs plots

--- photopipe/photometry/printhtml.py
+++ photopipe/photometry/printhtml.py
@@ -49,113 +49,6 @@ def plot_mag(filters, plotdict, colors):
 	pl.savefig('photcomp.plot.png', bbox_inches='tight')
 	pl.clf()
 
-def plot_seds_test():
-    filters = ['r','i','z','y','J','H']
-    colors = ['black', 'purple','blue','aqua','green','orange','red','yellow', 'magenta']
-    plot_seds(filters, colors)
-
-def plot_seds_1(filters, colors):
-    wavelengths = { 'r': 6122.3, 'i': 7439.5, 'z': 8897.1, 'y': 10289.4, 'J': 12350.0, 'K': 21590.0, 'u': 3594.9, 'g': 4640.4, 'H':16620.0 }
-    file_mask = ['index', 'RA', 'DEC', 'u', 'g', 'r', 'i', 'z', 'y', 'B', 'V', 'R', 'I', 'J', 'H', 'K', 'u_err', 'g_err', 'r_err', 'i_err', 'z_err', 'y_err', 'B_err', 'V_err', 'R_err', 'I_err', 'J_err', 'H_err', 'K_err', 'Mode']
-
-    files = glob.glob('seds/*.seds.txt')
-
-    if len(files) == 0:
-        print 'Did not find any SEDs files! Check your data directory path!'
-        return
-
-    color_counter = 0
-    for file in files:
-        print file
-
-        filter = file.split('/')[-1].split('.')[0]
-        if filter == 'Z' or filter == 'Y':
-            filter = filter.lower()
-
-        data = np.loadtxt(file, unpack=False)
-        filter_sources = {}
-
-        for i in range(len(data)):
-            source_index = str(int(data[i][0]))
-            filter_sources[source_index] = np.array([data[i]]) if not (source_index in filter_sources) else np.append(filter_sources[source_index], np.array([data[i]]), 0)
-
-        for source_index, seds in filter_sources.iteritems():
-            pl.figure()
-
-            for i, sed in enumerate(seds): # Assuming len = 1
-                print 'source: ' + source_index + ' sed: ' + str(i)
-                plot_x = []
-                plot_y = []
-
-                for f in filters:
-                    plot_x.append(wavelengths[f])
-                    plot_y.append(sed[file_mask.index(f)])
-                print plot_y
-                pl.plot(plot_x, plot_y, label=filter + '_' + str(i), color=colors[color_counter])
-
-            pl.xlabel(r'Wavelength ($\AA$)')
-            pl.ylabel('Magnitude')
-            pl.legend(loc='lower right')
-            pl.savefig('seds/' + source_index + '_' + filter + '.plot.png', bbox_inches='tight')
-            pl.clf()
-
-        color_counter = color_counter + 1
-
-def plot_seds_no_index(filters, colors):
-    wavelengths = { 'r': 6122.3, 'i': 7439.5, 'z': 8897.1, 'y': 10289.4, 'J': 12350.0, 'K': 21590.0, 'u': 3594.9, 'g': 4640.4, 'H':16620.0 }
-    file_mask = ['RA', 'DEC', 'u', 'g', 'r', 'i', 'z', 'y', 'B', 'V', 'R', 'I', 'J', 'H', 'K', 'u_err', 'g_err', 'r_err', 'i_err', 'z_err', 'y_err', 'B_err', 'V_err', 'R_err', 'I_err', 'J_err', 'H_err', 'K_err', 'Mode', 'Timestamp']
-
-    files = glob.glob('seds/*.seds.txt')
-
-    if len(files) == 0:
-        print 'Did not find any SEDs files! Check your data directory path!'
-        return
-
-    pl.figure()
-    pl.ylim([15,22])
-    pl.xlim([3000, 22000])
-
-    for file in files:
-        print file
-
-        filter = file.split('/')[-1].split('.')[0]
-        if filter == 'Z' or filter == 'Y':
-            filter = filter.lower()
-
-        data = np.genfromtxt(file, delimiter=' ', dtype=None)
-        filter_sources = {}
-
-        for i in range(len(data)):
-            source_index = str(data[i][0]) + '_' + str(data[i][1])
-            filter_sources[source_index] = np.array([data[i]]) if not (source_index in filter_sources) else np.append(filter_sources[source_index], np.array([data[i]]), 0)
-
-        for source_index, seds in filter_sources.iteritems():
-            ax = pl.subplot(111)
-
-            for i, sed in enumerate(seds): # Assuming len = 1
-                print 'source: ' + source_index + ' sed: ' + str(i)
-                plot_x = []
-                plot_y = []
-
-                for f in filters:
-                    plot_x.append(wavelengths[f])
-                    plot_y.append(sed[file_mask.index(f)])
-                print plot_y
-                ax.plot(plot_x, plot_y, label=sed[file_mask.index('Timestamp')])
-
-            pl.title(source_index.replace('_', ', ') + ' (' + filter + ')')
-            pl.xlabel(r'Wavelength ($\AA$)')
-            pl.ylabel('Magnitude')
-            # https://stackoverflow.com/a/4701285
-            # Shrink current axis's height by 10% on the bottom
-            box = ax.get_position()
-            ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                 box.width, box.height * 0.9])
-            # Put a legend below current axis
-            ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, shadow=True, ncol=5)
-            pl.savefig('seds/' + source_index + '_' + filter + '.plot.png', bbox_inches='tight')
-            pl.clf()
-
 def plot_seds(filters):
     wavelengths = { 'r': 6122.3, 'i': 7439.5, 'z': 8897.1, 'y': 10289.4, 'J': 12350.0, 'K': 21590.0, 'u': 3594.9, 'g': 4640.4, 'H':16620.0 }
     file_mask = ['RA', 'DEC', 'u', 'g', 'r', 'i', 'z', 'y', 'B', 'V', 'R', 'I', 'J', 'H', 'K', 'u_err', 'g_err', 'r_err', 'i_err', 'z_err', 'y_err', 'B_err', 'V_err', 'R_err', 'I_err', 'J_err', 'H_err', 'K_err', 'Mode', 'Timestamp', 'Filter']
@@ -189,7 +82,8 @@ def plot_seds(filters):
         ncols = (2 if len(filters_map) > 1 else 1)
         nrows = (int(len(filters_map) / 2) if len(filters_map) > 1 else 1)
         fig, ax = pl.subplots(nrows=nrows, ncols=ncols, figsize=(6 * ncols, 3 * nrows + 1))
-        pl.subplots_adjust(top=0.8, hspace=0.3, wspace=0.3)
+        # pl.subplots_adjust(top=0.8, hspace=0.3, wspace=0.3) with top title
+        pl.subplots_adjust(hspace=0.3, wspace=0.3)
 
         ax_row = 0
         ax_col = 0 if len(filters_map) > 2 else 1
@@ -226,9 +120,13 @@ def plot_seds(filters):
             else:
                 ax_col = ax_col + 1
 
-        pl.suptitle('Source #' + source_index, fontsize=16)        
+        # pl.suptitle('Source #' + source_index, fontsize=16)        
         pl.savefig('seds/' + source_index + '.plot.png', bbox_inches='tight')
         pl.clf()
+
+def plot_seds_test():
+    filters = ['r','i','z','y','J','H']
+    plot_seds(filters)
 
 def generate_headers_table(headers, headers_names):
 	t = '<table border="1"><tr>'
